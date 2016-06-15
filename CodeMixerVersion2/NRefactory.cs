9e4abff332106eaf7a0f2b17104e49772b5a52ba
John Morgan <johnmorgan032@btinternet.com>
2016-06-15 18:38:08 +0100

    added NRefactory claas for findinf the serctions in the code files I care about

---
+++ CodeMixerVersion2/NRefactory.cs
@@ -0,0 +1,89 @@
+﻿using ICSharpCode.NRefactory.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public class NRefactory
+    {
+        //####################################################################
+        //
+        //
+        // NRefactory Code Finders
+        //
+        //####################################################################
+        public  SyntaxTree ParseCode(string sourceCode)
+        {
+            CSharpParser parser = new CSharpParser();
+            SyntaxTree syntaxTree = parser.Parse(sourceCode);
+            return syntaxTree;
+        }
+
+        public  IEnumerable<NamespaceDeclaration> FindNameSpaces(SyntaxTree syntaxTree)
+        {
+            IEnumerable<NamespaceDeclaration> resuilt = syntaxTree.Descendants.OfType<NamespaceDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<ConstructorDeclaration> FindConstructor(SyntaxTree syntaxTree)
+        {
+            IEnumerable<ConstructorDeclaration> resuilt = syntaxTree.Descendants.OfType<ConstructorDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<MethodDeclaration> FindMethods(SyntaxTree syntaxTree)
+        {
+            IEnumerable<MethodDeclaration> resuilt = syntaxTree.Descendants.OfType<MethodDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<PropertyDeclaration> FindProopertys(SyntaxTree syntaxTree)
+        {
+            IEnumerable<PropertyDeclaration> resuilt = syntaxTree.Descendants.OfType<PropertyDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<EnumMemberDeclaration> FindEnums(SyntaxTree syntaxTree)
+        {
+            IEnumerable<EnumMemberDeclaration> resuilt = syntaxTree.Descendants.OfType<EnumMemberDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<VariableDeclarationStatement> FindVariables(SyntaxTree syntaxTree)
+        {
+            IEnumerable<VariableDeclarationStatement> resuilt = syntaxTree.Descendants.OfType<VariableDeclarationStatement>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+        public  IEnumerable<FieldDeclaration> FindFields(SyntaxTree syntaxTree)
+        {
+            IEnumerable<FieldDeclaration> resuilt = syntaxTree.Descendants.OfType<FieldDeclaration>();
+            if (resuilt != null)
+            {
+                return resuilt;
+            }
+            return null;
+        }
+    }
+}
--- CodeMixerVersion2/Program.cs
+++ CodeMixerVersion2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ICSharpCode.NRefactory.CSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,91 @@ namespace CodeMixerVersion2
 {
     class Program
     {
+        private static Rule _rule;
+        private static NRefactory _refactory;
+
         static void Main(string[] args)
         {
+            _rule = new Rule();
+            _refactory = new NRefactory();
+
+            List<SourceFile> sourceFiles = new List<SourceFile>();
+            List<NameConvert> convertedNames = new List<NameConvert>();
+
+            //find all .cs files in the dir
+            foreach (string file in FindFiles("C:\\Test"))
+            {
+                string sourceCode = ReadSourceFile(file);
+                SyntaxTree syntaxTree = _refactory.ParseCode(sourceCode);
+
+                //build our object
+                SourceFile sourceFile = BuildSourceFile(file, syntaxTree, _refactory);
+
+                //add to list of files we can fuck up with
+                sourceFiles.Add(sourceFile);
+            }
+
+            // check if there are some files to change
+            if (sourceFiles.Count > 0)
+            {
+                //convert all names to some random text
+                _rule.ConvertNames(sourceFiles, convertedNames);
+
+                //time to update our source files with the new names
+                foreach (SourceFile file in sourceFiles)
+                {
+                    //read in the original file
+                    string oldCode = null;
+                    using (StreamReader reader = new StreamReader(file.FileName))
+                    {
+                        oldCode = reader.ReadToEnd();
+                    }
+
+                    //loop throw converted names and replace in file
+                    foreach (NameConvert name in convertedNames)
+                    {
+                        oldCode = oldCode.Replace(name.oldName, name.NewName);
+                    }
+
+                    // Do some checking here to see if still vailed
+                    if (_refactory.ParseCode(oldCode) != null)
+                    {
+                        //wite changes back to file
+                        using (StreamWriter writer = new StreamWriter(file.FileName))
+                        {
+                            writer.Write(oldCode);
+                            writer.Flush();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static SourceFile BuildSourceFile(string file, SyntaxTree syntaxTree, NRefactory refactory)
+        {
+            SourceFile sourceFile = new SourceFile(file);
+            sourceFile.NameSpaces = refactory.FindNameSpaces(syntaxTree).ToList();
+            sourceFile.Constructors = refactory.FindConstructor(syntaxTree).ToList();
+            sourceFile.Methods = refactory.FindMethods(syntaxTree).ToList();
+            sourceFile.Propertys = refactory.FindProopertys(syntaxTree).ToList();
+            sourceFile.Enums = refactory.FindEnums(syntaxTree).ToList();
+            sourceFile.Variables = refactory.FindVariables(syntaxTree).ToList();
+            sourceFile.Fields = refactory.FindFields(syntaxTree).ToList();
+            return sourceFile;
+        }
+
+        private static IEnumerable<string> FindFiles(string location)
+        {
+            return Directory.GetFiles(location, "*.cs", SearchOption.AllDirectories);
+        }
+
+        private static string ReadSourceFile(string file)
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                return reader.ReadToEnd();
+            }
+            return null;
         }
     }
 }
