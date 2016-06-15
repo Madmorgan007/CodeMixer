6f6618e35c24f92e7387703073e5224fd663faca
John Morgan <johnmorgan032@btinternet.com>
2016-06-15 17:55:50 +0100

    Added rule base class

---
+++ CodeMixerVersion2/IRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public interface IRule
+    {
+        void Process(string text);
+    }
+}
