f1a1f57722bff9f5d217d6a4123c0116d9ba7c55
John Morgan <johnmorgan032@btinternet.com>
2016-06-15 18:05:11 +0100

    To hold old and new names of methods, classes, ect

---
+++ CodeMixerVersion2/NameConvert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public class NameConvert
+    {
+        public string oldName { get; set; }
+        public string NewName { get; set; }
+    }
+}
