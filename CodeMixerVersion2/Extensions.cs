d02039d52d8ba9192c5e69c6fc7e6365c9308196
John Morgan <johnmorgan032@btinternet.com>
2016-06-15 18:01:00 +0100

    added a string encryption class and a extensions class to hold pices of resuable code

---
+++ CodeMixerVersion2/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public static class Extensions
+    {
+        public static string Clean(this string str)
+        {
+            string newStr = str.Replace("/", string.Empty).Replace("=", string.Empty).Replace("+", string.Empty)
+                .Replace("-", string.Empty).Replace("*", string.Empty).Replace("&", string.Empty);
+
+            return newStr;
+        }
+    }
+}
---
+++ CodeMixerVersion2/Mixer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public class Mixer
+    {
+        private readonly byte[] _bytes = new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 };
+
+        public string Encode(string text)
+        {
+            string tempPassword = DateTime.Now.ToShortDateString();
+
+            return Encrypt(text, tempPassword);
+        }
+
+        private string Encrypt(string clearText, string password)
+        {
+            string encrypted = null;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, _bytes);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    encrypted = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return encrypted.Clean();
+        }
+    }
+}
