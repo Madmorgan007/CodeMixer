b7478209c059fb7bde1f2ab9decfc174ec402636
John Morgan <johnmorgan032@btinternet.com>
2016-06-15 18:32:43 +0100

    added code to find the serctions of code that I care about

---
+++ CodeMixerVersion2/Rule.cs
@@ -0,0 +1,93 @@
+﻿using ICSharpCode.NRefactory.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMixerVersion2
+{
+    public class Rule
+    {
+        public void ConvertNames(List<SourceFile> sourceFiles, List<NameConvert> convertedNames)
+        {
+            foreach (SourceFile file in sourceFiles)
+            {
+                #region NameSpaces
+                foreach (NamespaceDeclaration name in file.NameSpaces)
+                {
+                    if (!convertedNames.Any(n => n.oldName == name.Name))
+                    {
+                        convertedNames.Add(new NameConvert() { oldName = name.Name, NewName = Mixer.Encoder(name.Name) });
+                    }
+                }
+                #endregion
+
+                #region Constructors
+                foreach (ConstructorDeclaration constructor in file.Constructors)
+                {
+                    if (!convertedNames.Any(n => n.oldName == constructor.Name))
+                    {
+                        convertedNames.Add(new NameConvert() { oldName = constructor.Name, NewName = Mixer.Encoder(constructor.Name) });
+                    }
+                }
+                #endregion
+
+                #region Methods
+                foreach (MethodDeclaration method in file.Methods)
+                {
+                    if (!convertedNames.Any(n => n.oldName == method.Name))
+                    {
+                        convertedNames.Add(new NameConvert() { oldName = method.Name, NewName = Mixer.Encoder(method.Name) });
+                    }
+                }
+                #endregion
+
+                #region Propertys
+                foreach (PropertyDeclaration property in file.Propertys)
+                {
+                    if (!convertedNames.Any(n => n.oldName == property.Name))
+                    {
+                        convertedNames.Add(new NameConvert() { oldName = property.Name, NewName = Mixer.Encoder(property.Name) });
+                    }
+                }
+                #endregion
+
+                #region Enums
+                foreach (EnumMemberDeclaration enumvalue in file.Enums)
+                {
+                    if (!convertedNames.Any(n => n.oldName == enumvalue.Name))
+                    {
+                        convertedNames.Add(new NameConvert() { oldName = enumvalue.Name, NewName = Mixer.Encoder(enumvalue.Name) });
+                    }
+                    //add its perent the enum name
+                    string enumName = enumvalue.PrevSibling.PrevSibling.PrevSibling.PrevSibling.GetText();
+                    if (!string.IsNullOrEmpty(enumName))
+                    {
+                        if (!string.IsNullOrWhiteSpace(enumName))
+                        {
+                            if (!convertedNames.Any(n => n.oldName == enumName))
+                            {
+                                convertedNames.Add(new NameConvert() { oldName = enumName, NewName = Mixer.Encoder(enumName) });
+                            }
+                        }
+                    }
+                }
+                #endregion
+
+                #region Fields
+                foreach (FieldDeclaration field in file.Fields)
+                {
+                    foreach (VariableInitializer item in field.Variables)
+                    {
+                        if (!convertedNames.Any(n => n.oldName == item.Name))
+                        {
+                            convertedNames.Add(new NameConvert() { oldName = item.Name, NewName = Mixer.Encoder(item.Name) });
+                        }
+                    }
+                }
+                #endregion
+            }
+        }
+    }
+}
